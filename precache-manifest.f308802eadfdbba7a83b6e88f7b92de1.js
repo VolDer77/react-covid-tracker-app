874e462ffe3af317d2be55379d2dc28ae3fe6f92
volder <vlodyk00@gmail.com>
2020-09-04 14:13:56 +0300

    Updates

---
+++ precache-manifest.f308802eadfdbba7a83b6e88f7b92de1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90ca382d7b994e594611d880c95e2e7",
+    "url": "/react-covid-tracker-app/index.html"
+  },
+  {
+    "revision": "0a500d23c0b30af8183b",
+    "url": "/react-covid-tracker-app/static/css/main.9280f400.chunk.css"
+  },
+  {
+    "revision": "8d15ece370d182dd78b0",
+    "url": "/react-covid-tracker-app/static/js/2.99c59cbb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-covid-tracker-app/static/js/2.99c59cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a500d23c0b30af8183b",
+    "url": "/react-covid-tracker-app/static/js/main.e3e967b5.chunk.js"
+  },
+  {
+    "revision": "7efac04606cd2c61af37",
+    "url": "/react-covid-tracker-app/static/js/runtime-main.660c4c05.js"
+  }
+]);
