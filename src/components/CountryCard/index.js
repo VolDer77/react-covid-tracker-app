46dfb4d8d56bbf0cea2599fbe03836d4f19cfb75
volder <vlodyk00@gmail.com>
2020-09-04 14:00:01 +0300

    git hub pages added

--- src/components/CountryCard/index.js
+++ src/components/CountryCard/index.js
@@ -19,22 +19,22 @@ export default function CountryCard(props) {
           <h2 className="country__name">{countryName}</h2>
           <div className="country__info">
             <h3>
-              Total confirmed: <span>{totalCases}</span>
+              Total confirmed <span>{totalCases}</span>
             </h3>
             <h3>
-              New confirmed: <span>{newCases}</span>
+              New confirmed <span>{newCases}</span>
             </h3>
             <h3>
-              Total deaths: <span>{totalDeaths}</span>
+              Total deaths <span>{totalDeaths}</span>
             </h3>
             <h3>
-              New deaths: <span>{newDeaths}</span>
+              New deaths <span>{newDeaths}</span>
             </h3>
             <h3>
-              Total recovered: <span>{totalRecovered}</span>
+              Total recovered <span>{totalRecovered}</span>
             </h3>
             <h3>
-              New recovered: <span>{newRecovered}</span>
+              New recovered <span>{newRecovered}</span>
             </h3>
           </div>
         </div>
